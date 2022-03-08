895963581a284b1a5a4cd596d8c98c1a7f44b761
regularjoe24 <joeabouserhal77777@gmail.com>
2022-03-08 23:31:35 +0200

    started customer model

---
+++ lib/models/customer.dart
@@ -0,0 +1,14 @@
+class Customer {
+  String address;
+  String email;
+  int id;
+  String phone;
+  String name;
+
+  Customer(
+      {required this.name,
+      required this.email,
+      required this.phone,
+      required this.address,
+      required this.id});
+}
